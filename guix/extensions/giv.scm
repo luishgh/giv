a27bfea6cf2d80fd08324d4f461be27f1fe30a1a
luishgh <51053603+luishgh@users.noreply.github.com>
2022-05-08 21:45:07 -0300

    Add giv.scm
    * guix/extensions/giv.scm: New file.
    * .envrc: New file.

---
+++ guix/extensions/giv.scm
@@ -0,0 +1,48 @@
+(define-module (guix extensions giv)
+  #:use-module (guix scripts)
+  #:use-module (guix i18n)
+  #:use-module (guix diagnostics)
+  #:use-module (guix build utils)
+
+  #:use-module (ice-9 match)
+
+  #:export (guix-giv))
+
+(define (show-subcommands)
+  (display (G_ "Available commands:\n"))
+  (display (G_ "
+    init    bootstrap a Guix project")))
+
+(define (show-flags)
+  (display (G_ "
+  -m, --manifest=FILE    create project with the manifest from FILE"))
+  (display (G_ "
+  -n, --dry-run          do not actually build the modules"))
+
+  (newline)
+
+  (display (G_ "
+  -h, --help             display this help and exit"))
+  (display (G_ "
+  -V, --version          display version information and exit")))
+
+(define (show-help)
+  (display (G_ "Usage: guix giv COMMAND [OPTION]...
+Easy dependency management for Guix projects.\n"))
+  (newline)
+  (show-subcommands)
+  (newline)
+  (show-flags)
+  (newline))
+
+(define-command (guix-giv . args)
+  (category development)
+  (synopsis "TODO")
+  (match args
+    ((or ("-h") ("--help"))
+     (show-help)
+     (exit 0))
+    ((command _ ...)
+     (leave (G_ "~a: unknown sub-command~%") command))
+    (()
+     (leave (G_ "missing sub-command~%")))))
